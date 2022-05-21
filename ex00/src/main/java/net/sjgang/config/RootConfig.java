7292eb5c8ffe02b9bbfa696d8b9f8c15092d3850
sujeongHeo401 <gotnwjd40@gmail.com>
2022-05-22 02:37:49 +0900

    mybatis 테스트 코드

--- ex00/src/main/java/net/sjgang/config/RootConfig.java
+++ ex00/src/main/java/net/sjgang/config/RootConfig.java
@@ -2,6 +2,8 @@ package net.sjgang.config;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -25,6 +27,14 @@ public class RootConfig {
 		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
 		
 		return dataSource;
+	}
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception{
+		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+		sqlSessionFactory.setDataSource(dataSource());
+		
+		return (SqlSessionFactory) sqlSessionFactory.getObject();
 	}
 
 }
--- ex00/src/test/java/net/sjgang/persistence/DataSourceTests.java
+++ ex00/src/test/java/net/sjgang/persistence/DataSourceTests.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,26 +26,41 @@ public class DataSourceTests {
 	@Autowired
 	private DataSource ds;
 	
-	@Test
-	public void testConnection(){
-		
-		long start = System.currentTimeMillis();
-		for(int i = 0; i < 100; i++) {
-			try(Connection con = ds.getConnection()){
-				log.info(con);
-			}catch(Exception e) {
-				e.printStackTrace();
-				log.error(e.getMessage());
-			}
-		}
-		
-		long end = System.currentTimeMillis();
-		log.info("--------------");
-		log.info(end - start);
-	}
+//	@Test
+//	public void testConnection(){
+//		
+//		long start = System.currentTimeMillis();
+//		for(int i = 0; i < 100; i++) {
+//			try(Connection con = ds.getConnection()){
+//				log.info(con);
+//			}catch(Exception e) {
+//				e.printStackTrace();
+//				log.error(e.getMessage());
+//			}
+//		}
+//		
+//		long end = System.currentTimeMillis();
+//		log.info("--------------");
+//		log.info(end - start);
+//	}
 
 //	@Setter(onMethod_ = {@Autowired} )
-//	private DataSource dataSource;
+	private DataSource dataSource;
+//	
+	@Setter(onMethod_ = { @Autowired })
+	private SqlSessionFactory sqlSessionFactory;
+	
+	@Test
+	public void testMybatis() {
+		try(SqlSession session = sqlSessionFactory.openSession();
+				Connection con = session.getConnection();
+				) {
+					log.info(session);
+					log.info(con);
+				} catch(Exception e) {
+					fail(e.getMessage());
+				}
+	}
 //	@Test
 //	public void testConnection() {
 //		try(Connection con = dataSource.getConnection()){
