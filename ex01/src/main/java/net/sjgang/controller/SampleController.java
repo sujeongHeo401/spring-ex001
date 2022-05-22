698dde1298ecef1572b9ea5f99640b28335aef47
sujeongHeo401 <gotnwjd40@gmail.com>
2022-05-23 00:11:47 +0900

    "/ex01" api 추가 [GET]

--- ex01/src/main/java/net/sjgang/controller/SampleController.java
+++ ex01/src/main/java/net/sjgang/controller/SampleController.java
@@ -2,11 +2,13 @@ package net.sjgang.controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+//import org.springframework.web.bind.annotation.RequestParam;
+import net.sjgang.domain.SampleDTO;
 import lombok.extern.log4j.Log4j;
+
 @Controller
 @RequestMapping("/sample/*")
 @Log4j
@@ -28,5 +30,16 @@ public class SampleController {
 		log.info("basic only get........");
 		
 	}
+	
+	@GetMapping("/ex01")
+	public String ex01(SampleDTO dto) {
+		log.info(""+dto);
+		return "ex01";
+	}
+	
+//	@GetMapping("/ex02")
+//	public String ex02(@RequestParam("name") String name, @RequestParam("age") int age) {
+//		log.info("name : " + name);
+//	}
 	
 }
