9fc48913cf850d677111b3577a1f383c211ac856
sujeongHeo401 <gotnwjd40@gmail.com>
2022-05-23 03:40:54 +0900

    Post .. 뭔가 잘 안되는 느낌

--- ex01/src/main/java/net/sjgang/controller/SampleController.java
+++ ex01/src/main/java/net/sjgang/controller/SampleController.java
@@ -1,18 +1,24 @@
 package net.sjgang.controller;
 
+//import java.net.http.HttpHeaders;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -110,6 +116,32 @@ public class SampleController {
 		dto.setName("홍길동");
 		
 		return dto;
+	}
+	
+	@GetMapping("/ex07")
+	public ResponseEntity<String> ex07(){
+		log.info("/ex07............");
+		/// {"name" : "홍길동"}
+		String msg = "{\"name\": \"홍길동\"}";
+		
+		HttpHeaders header = new HttpHeaders();
+		header.add("Content-Type", "application/json;charset=UTF-8");
+		
+		return new ResponseEntity<>(msg, header, HttpStatus.OK);
+	}
+	
+	@GetMapping("/exUpload")
+	public void exUpload() {
+		log.info("/exupload.......");
+	}
+	
+	@PostMapping("/exUploadPost")
+	public void exUploadPost(ArrayList<MultipartFile> files) {
+		files.forEach(file -> {
+			log.info("----------------------------");
+			log.info("name: " + file.getOriginalFilename());
+			log.info("size:" + file.getSize());
+		});
 	}
 	
 }
