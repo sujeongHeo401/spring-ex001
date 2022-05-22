b90b489a9933cd2eced77a1a969e2cb99a4c4d5c
sujeongHeo401 <gotnwjd40@gmail.com>
2022-05-23 02:28:34 +0900

    @DateTimeFormat 사용

--- ex01/src/main/java/net/sjgang/controller/SampleController.java
+++ ex01/src/main/java/net/sjgang/controller/SampleController.java
@@ -25,11 +25,11 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class SampleController {
 	
-	@InitBinder
-	public void initBinder(WebDataBinder binder) {
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(dateFormat, false));
-	}
+//	@InitBinder
+//	public void initBinder(WebDataBinder binder) {
+//		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+//		binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(dateFormat, false));
+//	}
 	
 	@GetMapping("/ex03")
 	public String ex03(TodoDTO todo) {
