fe86d930dead21236a1ce79bfea0348081b2f9c2
sujeongHeo401 <gotnwjd40@gmail.com>
2022-05-29 17:35:22 +0900

    영속적인 crud 구현중 - insert, insertKey

--- ex02/src/main/java/net/sjgang/mapper/BoardMapper.java
+++ ex02/src/main/java/net/sjgang/mapper/BoardMapper.java
@@ -9,5 +9,7 @@ public interface BoardMapper {
 	
 	//@Select("select * from tbl_board where bno > 0")
 	public List<BoardVO> getList();
+	public void insert(BoardVO board);
+	public void insertSelectKey(BoardVO board);
 
 }
--- ex02/src/test/java/net/sjgang/mapper/BoardMapperTests.java
+++ ex02/src/test/java/net/sjgang/mapper/BoardMapperTests.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
+import net.sjgang.domain.BoardVO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
@@ -21,5 +22,30 @@ public class BoardMapperTests {
 	public void testGetList() {
 		mapper.getList().forEach(board -> log.info(board));
 	}
+	
+	@Test 
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글");
+		board.setContent("새로 작성하는 내용");
+		board.setWriter("newbie");
+		mapper.insert(board);
+		
+		log.info(board);
+	}
+	
+	@Test
+	public void testInsertSelectKey() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글 select key");
+		board.setContent("새로 작성하는 내용 select key");
+		board.setWriter("newbie");
+		
+		mapper.insertSelectKey(board);
+		
+		log.info(board);
+	}
+	
+	
 
 }
