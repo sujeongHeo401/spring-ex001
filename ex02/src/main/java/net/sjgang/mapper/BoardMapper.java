e00c2995864f7e02ab3ecc3da7ac23807d14224d
sujeongHeo401 <gotnwjd40@gmail.com>
2022-05-29 17:43:10 +0900

    read 추가

--- ex02/src/main/java/net/sjgang/mapper/BoardMapper.java
+++ ex02/src/main/java/net/sjgang/mapper/BoardMapper.java
@@ -9,7 +9,11 @@ public interface BoardMapper {
 	
 	//@Select("select * from tbl_board where bno > 0")
 	public List<BoardVO> getList();
+	
 	public void insert(BoardVO board);
+	
 	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
 
 }
