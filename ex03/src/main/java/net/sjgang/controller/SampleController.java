75057884f7516a168bdec2949416de25a02589b5
sujeongHeo401 <gotnwjd40@gmail.com>
2023-01-24 18:50:46 +0900

    rest 방식으로 새로 풀어버리기

---
+++ ex03/src/main/java/net/sjgang/controller/SampleController.java
@@ -0,0 +1,34 @@
+package net.sjgang.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.log4j.Log4j;
+import net.sjgang.domain.SampleVO;
+
+@RestController
+@RequestMapping("/sample")
+@Log4j
+public class SampleController {
+	
+	
+	@GetMapping(value="/getSample"
+			,produces = { MediaType.APPLICATION_JSON_VALUE,
+						MediaType.APPLICATION_XML_VALUE })
+	public SampleVO getSample() {
+		return new SampleVO(112, "스타", "로드");
+	}
+	@GetMapping(value ="/getText", produces ="text/plain; charset=UTF-8")
+	public String getText() {
+		log.info("MIME TYPE: " + MediaType.TEXT_PLAIN_VALUE);
+		return "안녕하세요";
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
